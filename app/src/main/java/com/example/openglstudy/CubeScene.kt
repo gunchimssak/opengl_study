bfe11b80334d56b408f4dbcd083053ae7e4ff0ac
yhlee2 <yhlee2@rsupport.com>
2023-01-04 14:32:24 +0900

    큐브 생성2

--- app/src/main/java/com/example/openglstudy/CubeScene.kt
+++ app/src/main/java/com/example/openglstudy/CubeScene.kt
@@ -3,10 +3,14 @@ package com.example.openglstudy
 import android.content.Context
 import android.opengl.GLES20
 import android.opengl.GLES30.*
+import android.opengl.Matrix
+import android.util.Log
 import com.example.openglstudy.common.*
 import com.example.openglstudy.common.loadBitmap
 import glm_.glm
 import glm_.mat4x4.Mat4
+import glm_.toLong
+import glm_.vec2.Vec2
 import glm_.vec3.Vec3
 
 class CubeScene(
@@ -77,32 +81,37 @@ class CubeScene(
         glActiveTexture(GL_TEXTURE0)
         glBindTexture(GL_TEXTURE_2D, texture1.getId())
         program.use()
-
-//        val view = glm.translate(Mat4(1f), Vec3(0f, 0f, 10f))
-//        GLES20.glUniformMatrix4fv(
-//            program.getUniformLocation("view"),
-//            1,
-//            true,
-//            toFloatArray(view),
-//            0
-//        )
-//        val projection =
-//            glm.perspective(glm.PIf * 0.25f, width.toFloat() / height.toFloat(), 0.1f, 100f)
-//        GLES20.glUniformMatrix4fv(
-//            program.getUniformLocation("projection"),
-//            1,
-//            true,
-//            toFloatArray(projection),
-//            0
-//        )
+        val viewF = FloatArray(16) { 0f }
+        Matrix.setIdentityM(viewF, 0)
+        Matrix.translateM(viewF, 0, 0f, 0f, -5f)
+        Log.e("view", "${viewF.toList()}")
+        GLES20.glUniformMatrix4fv(
+            program.getUniformLocation("view"),
+            1,
+            false,
+            viewF,
+            0
+        )
+        val proj = FloatArray(16) { 0f }
+        Matrix.setIdentityM(proj, 0)
+        Matrix.perspectiveM(proj, 0, 45f, width.toFloat() / height, .1f, 10000f)
+        GLES20.glUniformMatrix4fv(
+            program.getUniformLocation("projection"),
+            1,
+            false,
+            proj,
+            0
+        )
     }
 
     override fun draw() {
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f)
         glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
         glBindVertexArray(vertexData.getVaoId())
+        val model = FloatArray(16) { 0f }
+       // Matrix.setRotateM(model,0,3f,1f,2f,0f)
         var trans = Mat4()
-        trans = glm.rotate(trans, timer.sinceStartSecs(), Vec3(0, 1, 2))
+        trans = glm.rotate(trans, timer.sinceStartSecs(), Vec3(3, 1, 2))
         GLES20.glUniformMatrix4fv(
             program.getUniformLocation("model"),
             1,
